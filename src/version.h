57a013d93fb12bf7e59cd962a4dc0e8aa5837d55
anak10thn <anak10thn@gmail.com>
2015-10-05 01:54:43 +0700

    Bump version to 0.2 Copper Shark

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef IGNSDK_VERSION_H
 #define IGNSDK_VERSION_H
 
-#define IGNSDK_VERSION "0.1.3"
-#define IGNSDK_CODENAME "Basking Shark"
+#define IGNSDK_VERSION "0.2.0"
+#define IGNSDK_CODENAME "Copper Shark"
 #define IGNSDK_MAINTAINER "Ibnu Yahya <anak10thn@gmail.com>"
 
 #endif
